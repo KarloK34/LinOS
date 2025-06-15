5b944a2ef1beb0ee8332ca7711eb5efbc1e38f8e
KarloK34 <karlokraml2@gmail.com>
2025-06-15 22:10:34 +0200

    Set device language as apps default

--- lib/core/locale/cubit/locale_cubit.dart
+++ lib/core/locale/cubit/locale_cubit.dart
@@ -5,14 +5,22 @@ import 'package:linos/l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LocaleCubit extends Cubit<Locale> {
-  LocaleCubit()
-    : super(
-        AppLocalizations.supportedLocales.firstWhere(
-          (locale) => locale.languageCode == 'hr',
-          orElse: () => AppLocalizations.supportedLocales.first,
-        ),
-      ) {
+  LocaleCubit() : super(_getDeviceLocale()) {
     _loadLocale();
+  }
+
+  static Locale _getDeviceLocale() {
+    final deviceLocale = PlatformDispatcher.instance.locale;
+
+    final supportedLocale = AppLocalizations.supportedLocales.firstWhere(
+      (locale) => locale.languageCode == deviceLocale.languageCode,
+      orElse: () => AppLocalizations.supportedLocales.firstWhere(
+        (locale) => locale.languageCode == 'hr',
+        orElse: () => AppLocalizations.supportedLocales.first,
+      ),
+    );
+
+    return supportedLocale;
   }
 
   void changeLocale(Locale locale) async {
@@ -23,11 +31,16 @@ class LocaleCubit extends Cubit<Locale> {
 
   void _loadLocale() async {
     final prefs = await SharedPreferences.getInstance();
-    final code =
-        prefs.getString('locale') ??
-        AppLocalizations.supportedLocales
-            .firstWhere((locale) => locale.languageCode == 'hr', orElse: () => AppLocalizations.supportedLocales.first)
-            .languageCode;
-    emit(Locale(code));
+    final savedLocaleCode = prefs.getString('locale');
+
+    if (savedLocaleCode != null) {
+      final savedLocale = AppLocalizations.supportedLocales.firstWhere(
+        (locale) => locale.languageCode == savedLocaleCode,
+        orElse: () => _getDeviceLocale(),
+      );
+      emit(savedLocale);
+      return;
+    }
+    emit(_getDeviceLocale());
   }
 }
